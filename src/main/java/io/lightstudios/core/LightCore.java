59cd806ea71936e121d305b131baaf8529a00ca5
lightplugins <lightningdesign@outloo.de>
2024-11-19 13:22:11 +0100

    Initial commit

---
+++ src/main/java/io/lightstudios/core/LightCore.java
@@ -0,0 +1,22 @@
+package io.lightstudios.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LightCore extends JavaPlugin {
+
+
+    @Override
+    public void onLoad() {
+        // Plugin load logic
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
