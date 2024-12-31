78be1676a571320d5a8f51ff3ac5d63fa89fcce1
lightplugins <lightningdesign@outlook.de>
2024-12-31 12:24:08 +0100

    modified a proxy message and updated to 0.2.7

--- src/main/java/io/lightstudios/core/proxy/messaging/backend/send/SendProxyMessageRequest.java
+++ src/main/java/io/lightstudios/core/proxy/messaging/backend/send/SendProxyMessageRequest.java
@@ -6,12 +6,14 @@ import io.lightstudios.core.LightCore;
 import io.lightstudios.core.proxy.util.SubChannels;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class SendProxyMessageRequest {
 
-    public static void sendProxyMessage(Player player, String message) {
+    public static void sendProxyMessage(Player player, UUID uuid, String message) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF(SubChannels.MESSAGE_REQUEST.getId());
-        out.writeUTF(player.getUniqueId().toString());
+        out.writeUTF(uuid.toString());
         out.writeUTF(LightCore.instance.getColorTranslation().adventureTranslator(message, player));
 
         player.sendPluginMessage(LightCore.instance, "lightstudio:lightcore", out.toByteArray());
