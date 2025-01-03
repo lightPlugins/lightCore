d50fd87295742dc42063bd6a8dc5109fe3ea560f
lightplugins <lightningdesign@outloo.de>
2025-01-03 08:06:32 +0100

    updated core.yml file and updated to 0.3.4

--- src/main/java/io/lightstudios/core/util/files/configs/CoreSettings.java
+++ src/main/java/io/lightstudios/core/util/files/configs/CoreSettings.java
@@ -1,6 +1,9 @@
 package io.lightstudios.core.util.files.configs;
 
+import io.lightstudios.core.LightCore;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
 
 public class CoreSettings {
 
@@ -11,6 +14,29 @@ public class CoreSettings {
     }
 
     public String language() { return config.getString("language"); }
+
+    public String syncType() {
+        String data = config.getString("server-synchronisation.type");
+
+        if(data == null) {
+            LightCore.instance.getConsolePrinter().printError(List.of(
+                    "The server-synchronisation.type is not set in the core.yml",
+                    "Please set it to either 'redis' or 'mysql'",
+                    "and restart the server!"
+            ));
+            return "none";
+        }
+
+        if(!data.equalsIgnoreCase("redis") && !data.equalsIgnoreCase("mysql")) {
+            LightCore.instance.getConsolePrinter().printError(List.of(
+                    "The server-synchronisation.type is not set to a valid value in the core.yml",
+                    "Please set it to either 'redis' or 'mysql'",
+                    "and restart the server!"
+            ));
+            return "none";
+        }
+        return data;
+    }
 
     // redis server credentials
     public boolean redisEnabled() { return config.getBoolean("server-synchronisation.redis.enable"); }
