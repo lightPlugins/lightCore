191480af1b05156a2fa9b03b7b8c5f8219ff871b
lightplugins <lightningdesign@outloo.de>
2025-04-25 16:57:25 +0200

    prepare new Title system and removed the old one

---
+++ src/main/java/io/lightstudios/core/player/title/LightTitleManager.java
@@ -0,0 +1,7 @@
+package io.lightstudios.core.player.title;
+
+public class LightTitleManager {
+
+
+
+}
---
+++ src/main/java/io/lightstudios/core/player/title/models/LightTitleSettings.java
@@ -0,0 +1,12 @@
+package io.lightstudios.core.player.title.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LightTitleSettings {
+
+
+
+}
